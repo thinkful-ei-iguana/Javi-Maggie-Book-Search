7b676a51e4b8bc15d53a2617cdf45ee379b9550e
therealmbittarelli <maggiekmcclellan@gmail.com>
2019-11-18 13:56:16 -0500

    working on API

--- book-search-app/src/Components/search.js
+++ book-search-app/src/Components/search.js
@@ -9,36 +9,45 @@ class Search extends Component {
     };
   }
 
-
   handleSubmit(e) {
     e.preventDefault();
-    console.log('handleSubmit is working');
+    //need to use API key
+    const searchInput = encodeURIComponent(this.state.searchTerm);
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${searchInput}`
+    console.log('url is', url);
+    const options = {
+      method: 'GET',
+      body: JSON.stringify(searchInput),
+      headers: {
+        "Content-Type": "application/json"
+      }
+    }
   }
 
   searchTermEntered(searchTerm) {
-    
-    this.setState = ({
+    this.setState({
       searchTerm
     });
   }
   
   
   render() {
-    console.log('searchTerm in render', this.state.searchTerm);
+    console.log('this is searchTerm', this.state.searchTerm);
     return (
       <form className="search-input" onSubmit={e => this.handleSubmit(e)}>
         <label htmlFor="search">
-          <input type="text" onChange={e => this.searchTermEntered(e.target.value)} name="search" id="search" placeholder="Great Expectations">
+          <input 
+            type="text" 
+            onChange={e => this.searchTermEntered(e.target.value)} 
+            name="search" 
+            id="search" 
+            placeholder="Great Expectations">
           </input>
         </label>
-          <button type="submit" onClick={this.searchTermEntered}>Hit It!</button>
+          <button type="submit" onClick={() => this.searchTermEntered}>Hit It!</button>
       </form>
     )
-
   }
-
-
-
 }
 
 export default Search;
