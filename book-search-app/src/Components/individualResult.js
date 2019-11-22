27f99eb843bd13f80dbb9aab6348a8faaf50b28c
therealmbittarelli <maggiekmcclellan@gmail.com>
2019-11-21 22:03:46 -0500

    polished css

--- book-search-app/src/Components/individualResult.js
+++ book-search-app/src/Components/individualResult.js
@@ -2,22 +2,22 @@ import React, { Component } from 'react';
 import './individualResult.css';
 
 function IndividualResults(props) {
-  
-    return(
-      <div className="book-result">
-        <h2>{props.title}</h2>
-        <div className="image">
-          <img src={props.thumbnail} alt="Image of book" />
-        </div>
-        <div className="content">
-          <p>{props.author}</p>
-          <p>{props.description}</p>
-        </div>
-        <hr></hr>
+
+  return (
+    <div className="book-result">
+      <h3>{props.title}</h3>
+      <div className="image">
+        <img src={props.thumbnail} alt="Image of book" />
       </div>
-    )
+      <div className="content">
+        <p>{props.author}</p>
+        <p>{props.description}</p>
+      </div>
+      <hr></hr>
+    </div>
+  )
 }
 
- 
+
 
 export default IndividualResults;
