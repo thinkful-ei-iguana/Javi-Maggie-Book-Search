bad4ee5b09b7cd412998f012c896f403698f43ec
therealmbittarelli <maggiekmcclellan@gmail.com>
2019-11-21 22:08:40 -0500

    adding a bit more styling

--- book-search-app/src/Components/header.js
+++ book-search-app/src/Components/header.js
@@ -2,12 +2,12 @@ import React from 'react';
 import './header.css';
 
 function Header() {
-  
-    return (
-      <header>
-        <h1>Google Book Search</h1>
-      </header>
-    )
+
+  return (
+    <header>
+      <h1><span className="blue">G</span><span className="red">o</span><span id="orange">o</span><span className="blue">g</span><span id="green">l</span><span className="red">e</span> Book Search</h1>
+    </header>
+  )
 }
 
 export default Header;
